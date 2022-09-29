7225590a27f86b63e7d722708fd1d5ff342a41dc
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-09-29 07:09:41 +0545

    Adding schema

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,36 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 const returnSignupPage = (req, res) => {
-    res.render('signup')
-}
+  res.render("signup");
+};
 
 const returnLoginPage = (req, res) => {
-    res.render('login')
-}
+  res.render("login");
+};
 
 const createUser = async (req, res) => {
-
-}
+  try {
+    //Create a user in the db
+    const user = await User.create(req.body);
+    res.json({user: user._id})
+  } catch (err) {
+    res.json({ errors: {
+        email : "",
+        password: ""
+    } });
+  }
+};
 
 const loginUser = (req, res) => {
-    //Code
-}
+  //Code
+};
 
-const logoutUser = (req, res) => {
-    
-}
+const logoutUser = (req, res) => {};
 
 module.exports = {
-    returnSignupPage,
-    returnLoginPage,
-    createUser,
-    loginUser,
-    logoutUser
-}
+  returnSignupPage,
+  returnLoginPage,
+  createUser,
+  loginUser,
+  logoutUser,
+};

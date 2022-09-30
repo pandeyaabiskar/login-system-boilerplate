45b821704f603c5de429569efba7dc9869726c4d
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2022-09-30 08:19:27 +0545

    Auth completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const handleErrors = (err) => {
   const errors = {
@@ -7,15 +8,24 @@ const handleErrors = (err) => {
     password: "",
   };
 
-  if(err.code === 11000){
-    errors.email = "Email already exists"
+  if(err.message === "incorrect email"){
+    errors.email = "Email is incorrect"
+    return errors
+  }
+  if(err.message === "incorrect password"){
+    errors.password = "Password is incorrect"
+    return errors
+  }
+
+  if (err.code === 11000) {
+    errors.email = "Email already exists";
     return errors;
   }
 
   if (err._message === "User validation failed") {
-    Object.values(err.errors).map(({properties}) => {
-        errors[properties.path] = properties.message
-    })
+    Object.values(err.errors).map(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
   }
 
   return errors;
@@ -34,23 +44,47 @@ const createUser = async (req, res) => {
     //Create a user in the db
     const user = await User.create(req.body);
     //Create a token and send it in cookie
-    const token = jwt.sign({user: user._id}, process.env.JWT_SECRET, {
-        expiresIn: '1d'
-    })
+    const token = jwt.sign({ user: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
     res.cookie("jwt", token);
-    
+
     res.json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
-    res.json({ errors : errors });
+    res.json({ errors: errors });
   }
 };
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   //Code
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        const token = jwt.sign({ user: user._id }, process.env.JWT_SECRET, {
+          expiresIn: "1d",
+        });
+        res.cookie("jwt", token);
+        res.json({ user: user._id });
+      } else {
+        throw Error("incorrect password");
+      }
+    } else {
+      throw Error("incorrect email");
+    }
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.json({ errors: errors });
+  }
 };
 
-const logoutUser = (req, res) => {};
+const logoutUser = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
 
 module.exports = {
   returnSignupPage,
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,39 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
   //Middlewares
-  
+  const { jwt: token } = req.cookies;
+
+  //Check if user has a token
+  if (token) {
+    const isValid = jwt.verify(token, process.env.JWT_SECRET);
+    //Check if token is valid or not
+    if (isValid) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  } else {
+    res.redirect("/login");
+  }
 };
 
-module.exports = { requireAuth };
+const checkUser = (req, res, next) => {
+  const { jwt: token } = req.cookies;
+  res.locals.user = null;
+
+  if(token) {
+    jwt.verify(token, process.env.JWT_SECRET, async(err, decoded) => {
+      if(err) next();
+
+      const user = await User.findById(decoded.user);
+      res.locals.user = user;
+      next();
+    })
+  }else {
+    next()
+  }
+}
+
+module.exports = { requireAuth, checkUser };
